98552c90845bd9f19a6827c15371a445296c30c4
Jaromir Stana <stanaj@ag512-7.arcig>
2025-12-15 14:30:02 +0100

    uprava main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Python
 def pozdrav(jmeno):
-    return f"Ahoj, {jmeno}! Toto je verze 1.1."
+    return f"Ahoj, {jmeno}! Toto je verze 1.1. - upraveno"
 
 if __name__ == "__main__":
     print(pozdrav("Svět Gitu"))
